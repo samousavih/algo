67d82dad791ab5e25ba67592c2b901db1295f052
Exo Digital <exodigital@Unknown.gateway>
2021-07-25 15:07:25 +1000

    added two more alg

---
+++ sample/longest-common-subsequesnce/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lcs_efficent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lcsEfficent("ABCGDEFGHIG", "ABDCGHSIGk"));
+        }
+
+        static int lcsEfficent(string x, string y)
+        {
+            var c = new int[x.Length, y.Length];
+            for(var i = 0; i< x.Length; i++)
+            {
+                c[i, 0] = 0;
+            }
+            for (var i = 0; i < y.Length; i++)
+            {
+                c[0, i] = 0;
+            }
+            for(var i = 1; i< x.Length; i++)
+            {
+                for(var j = 1; j < y.Length; j++)
+                {
+                    if(x[i] == y[j])
+                    {
+                        c[i, j] = c[i - 1, j - 1] + 1;
+                    }
+                    else 
+                    {
+                        c[i, j] = Math.Max(c[i - 1, j], c[i, j - 1]);
+                    }
+                }
+            }
+            return c[x.Length-1,y.Length-1];
+        }
+    }
+}
---
+++ sample/longest-common-substring/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace lcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lcs("ABCGDEFGHIG", "ABDCGHSIGk"));
+        }
+
+        static int lcs(string x, string y)
+        {
+            var biggerLen = x.Length;
+            var smallerLen = y.Length;
+            var common = new int[smallerLen, smallerLen,biggerLen];
+
+            for (var i = 0; i < smallerLen; i++)
+            {
+                
+                for (var j = 0; j < smallerLen; j++)
+                {
+                    for (var k = 0; k < biggerLen; k++)
+                    {
+
+                        common[i, j, k] = -1;
+                    }
+
+                }
+            }
+
+            for (var i = 0; i < smallerLen; i++)
+            {
+                var k = 0;
+                for (var j = 0; j < biggerLen; j++)
+                {
+                    if (x[j] == y[i])
+                    {
+                        common[0, i,k] = j;
+                        k++;
+                    }
+                }
+            }
+            var maxComSub = 0;
+
+            for (var lcsLen = 1; lcsLen < smallerLen; lcsLen++)
+            {
+                for (var c = 0; c < smallerLen - 1; c++)
+                {
+                    for (var k = 0; k< biggerLen; k++) {
+                        if (common[lcsLen - 1, c,k] > -1)
+                        {
+                            if (common[lcsLen - 1, c,k] + 1 < biggerLen)
+                            {
+                                if (y[c + 1] == x[common[lcsLen - 1, c,k] + 1])
+                                {
+                                    common[lcsLen, c + 1,k] = common[lcsLen - 1, c,k] + 1;
+                                    maxComSub = lcsLen + 1;
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            return maxComSub;
+        }
+    }
+}
